a0bf345896c6007c20145db2bf020f02cc24a4d2
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-10-02 18:57:26 -0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./components/Header.jsx";
 import Entry from "./components/Entry.jsx";
+import data from "./data.js";
 
 function App() {
+
+  const entries = data.map(el => <Entry key={el.id} {...el}/>)
+
   return (
     <>
       <Header />
       <main className="container">
-        <Entry />
+        {entries}
       </main>
     </>
   );
